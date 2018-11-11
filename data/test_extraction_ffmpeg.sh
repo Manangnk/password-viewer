9c6ec6e7bcb6c818c888fc2865c88b7c73bc0e8a
Robert Cunningham <robertvcunningham@gmail.com>
2018-11-11 01:30:24 -0500

    Final work

--- data/test_extraction_ffmpeg.sh
+++ data/test_extraction_ffmpeg.sh
@@ -1,3 +1,3 @@
-ffmpeg -i person.mov -r 24 -f image2 test/person/%d.jpg
-cp test/screen/1.jpg test/screen/0.jpg
+ffmpeg -i test.mov -r 24 -f image2 test/person/%d.jpg
+cp test/person/1.jpg test/person/0.jpg
 
