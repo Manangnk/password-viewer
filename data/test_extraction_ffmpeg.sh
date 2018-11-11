beac20bf29ae7e1005952e9652d2275ba1bf9e35
Robert Cunningham <robertvcunningham@gmail.com>
2018-11-11 00:00:42 -0500

    its midnight

--- data/test_extraction_ffmpeg.sh
+++ data/test_extraction_ffmpeg.sh
@@ -1,5 +1,3 @@
 ffmpeg -i person.mov -r 24 -f image2 test/person/%d.jpg
-ffmpeg -i screen.mov -r 24 -f image2 test/screen/%d.jpg
-cp test/screen/1.jpg test/screen/0.jpg
 cp test/screen/1.jpg test/screen/0.jpg
 
